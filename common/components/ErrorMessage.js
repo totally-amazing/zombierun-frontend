fbe08d4983f4f5f8542072b839de7a0bd50b2a59
yohrran <johnyohan93@gmail.com>
2022-02-03 21:53:44 +0900

    feat: add errorMessage

---
+++ common/components/ErrorMessage.js
@@ -0,0 +1,19 @@
+import { showMessage } from 'react-native-flash-message';
+
+import COLORS from '../constants/COLORS';
+
+const DURATION = 3000;
+
+const showErrorMessage = (message) => {
+  showMessage({
+    message,
+    duration: DURATION,
+    style: {
+      alignItems: 'center',
+      backgroundColor: COLORS.DEEP_RED,
+      color: COLORS.WHITE,
+    },
+  });
+};
+
+export default showErrorMessage;
