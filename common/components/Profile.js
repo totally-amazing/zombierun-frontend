c550f838de47b4f003bda6a7aa66410a21d9602e
hyemin <hmpkk09@gmail.com>
2022-02-06 14:22:56 +0900

    feat: add Profile component

---
+++ common/components/Profile.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Image, Pressable, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
+
+import { useSelector } from 'react-redux';
+
+import PROFILE from '../constants/PROFILE';
+
+const Profile = ({ size, onPress }) => {
+  const imageUrl = useSelector((state) => state.user.imageUrl);
+
+  return (
+    <Pressable onPress={onPress}>
+      <Image
+        source={imageUrl || require('../../assets/avatar.jpeg')}
+        style={styles[size]}
+      />
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  small: {
+    width: PROFILE.SMALL,
+    height: PROFILE.SMALL,
+    borderRadius: PROFILE.SAMLL_BORDER_RADIUS,
+  },
+  medium: {
+    width: PROFILE.MEDIUM,
+    height: PROFILE.MEDIUM,
+    borderRadius: PROFILE.MEDIUM_BORDER_RADIUS,
+  },
+  large: {
+    width: PROFILE.LARGE,
+    height: PROFILE.LARGE,
+    borderRadius: PROFILE.LARGE_BORDER_RADIUS,
+  },
+});
+
+Profile.propTypes = {
+  size: PropTypes.oneOf(['small', 'medium', 'large']).isRequired,
+  onPress: PropTypes.func,
+};
+
+Profile.defaultProps = {
+  onPress: () => {},
+};
+
+export default Profile;
