7863ff63faadf9dde43654348f78bb9cf84866bc
hyemin <hmpkk09@gmail.com>
2022-02-12 11:35:16 +0900

    feat: remove authSlice

--- store/index.js
+++ store/index.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import authReducer from './authSlice';
 import userReducer from './userSlice';
 import gameReducer from './gameSlice';
 import uiReducer from './uiSlice';
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
     user: userReducer,
     game: gameReducer,
     ui: uiReducer,
